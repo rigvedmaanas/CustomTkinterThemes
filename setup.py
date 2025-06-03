f47472863f7c8b0cfc949d72524a6d9701e01f44
rigvedmaanas <rigvedmaanas@gmail.com>
2025-06-03 17:15:37 +0530

    Final Changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="CustomTkinterThemes",
-    version="0.2.0",
+    version="1.0.0",
     description="A theme manager for CustomTkinter",
     long_description=long_description,
     long_description_content_type="text/markdown",
